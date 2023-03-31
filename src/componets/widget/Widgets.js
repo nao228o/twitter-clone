964cb3244eb0dada91b3892f3dd17acf4cc2df97
対馬直人 <tsushimanaoto@tsushimanaotos-MacBook-Air.local>
2023-03-31 20:28:54 +0900

    Update Widgets.js

--- src/componets/widget/Widgets.js
+++ src/componets/widget/Widgets.js
@@ -1,5 +1,9 @@
 import { Search } from '@mui/icons-material'
 import React from 'react'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, 
+    TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, 
+    TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton 
+} from 'react-twitter-embed';
 
 function Widgets() {
   return (
@@ -11,6 +15,20 @@ function Widgets() {
 
       <div className='widgets--widgetContainer'>
         <h2>いまどうしてる？</h2>
+
+        <TwitterTweetEmbed tweetId='1583061520069513216'/>
+
+        <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="saurabhnemade"
+            options={{height: 400}}
+        />
+
+        <TwitterShareButton
+            url={'https://twitter.com/Shin_Engineer'}
+            options={{ text: '#reactjs JS勉強中', via: 'Shin_Engineer' }}
+        />
+
       </div>
     </div>
   )
