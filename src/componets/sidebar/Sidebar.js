354721c5ce4ea024d32e0026d1c6f82f78685340
対馬直人 <tsushimanaoto@tsushimanaotos-MacBook-Air.local>
2023-03-31 16:20:07 +0900

    update sidebar

--- src/componets/sidebar/Sidebar.js
+++ src/componets/sidebar/Sidebar.js
@@ -2,6 +2,14 @@ import React from 'react'
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MailIcon from '@mui/icons-material/Mail';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
 
 function Sidebar() {
   return (
@@ -11,20 +19,17 @@ function Sidebar() {
       
       {/* SidebarOption */}
       <SidebarOption text='ホーム' Icon={ HomeIcon }/>
-      <SidebarOption text='話題を検索' Icon={ HomeIcon }/>
-      <SidebarOption text='通知' Icon={ HomeIcon }/>
-      <SidebarOption text='ブックマーク' Icon={ HomeIcon }/>
-      <SidebarOption text='リスト' Icon={ HomeIcon }/>
-      <SidebarOption text='プロフィール' Icon={ HomeIcon }/>
-      <SidebarOption text='もっと見る' Icon={ HomeIcon }/>
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption text='話題を検索' Icon={ SearchIcon }/>
+      <SidebarOption text='通知' Icon={ NotificationsIcon }/>
+      <SidebarOption text='メッセージ' Icon={ MailIcon }/>
+      <SidebarOption text='ブックマーク' Icon={ BookmarkIcon }/>
+      <SidebarOption text='リスト' Icon={ FormatListBulletedIcon }/>
+      <SidebarOption text='プロフィール' Icon={ PermIdentityIcon }/>
+      <SidebarOption text='もっと見る' Icon={ MoreHorizIcon }/>
 
 
       {/* ツイートボタン */}
+        <Button variant='outlined' className='sidebar--tweet'>ツイートする</Button>
     </div>
   )
 }
