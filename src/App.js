628335e9771a4135b42cdc1d0c5bc21161bd411f
対馬直人 <tsushimanaoto@tsushimanaotos-MacBook-Air.local>
2023-03-31 16:56:13 +0900

    create timeline.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Sidebar from './componets/sidebar/Sidebar';
+import Timeline from './componets/timeline/Timeline';
 
 function App() {
   return <div className='app'>
     <Sidebar />
+
+    {/* Timeline */}
+    <Timeline />
   </div>
 }
 
---
+++ src/componets/timeline/Timeline.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      {/* {Header} */}
+        <div className='timeline--header'>
+            <h2>ホーム</h2>
+        </div>
+      {/* {TweetBox} */}
+
+
+      {/* {Post} */}
+      {/* {Post} */}
+      {/* {Post} */}
+      {/* {Post} */}
+      {/* {Post} */}
+      {/* {Post} */}
+      {/* {Post} */}
+      </div>
+  )
+}
+
+export default Timeline
